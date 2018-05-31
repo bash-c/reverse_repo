8ddaaf22ea9fa20124b6964c263950d3ce274ed4
M4xW4n9 <aboultraman@gmail.com>
2018-05-31 09:52:47 +0800

    unfinished

--- inndy_a-maze/solve.py
+++ inndy_a-maze/solve.py
@@ -4,23 +4,22 @@ __Auther__ = 'M4x'
 
 from string import printable
 
+# xxd -e -g 4 ./map| cut -d' ' -f2-5 > hexmap
 with open("./hexmap") as f:
     m = f.read().split()
+    #  print len(m)
 
-m = [int(i, 16) for i in m]
-#  LODWORD(v2) = *(_DWORD *)&input_map[512 * v2 + 4 * (*hidden_message & 0x7F)];
-#  print len(m), type(m)
+m = [int(m[i], 16) for i in xrange(len(m)) if i % 4 == 0]
+#  print m, len(m)
 #  print set(m), len(set(m))
+#  print m.count(0xffffffff)
+for idx, c in enumerate(m):
+    if c == 0xffffffff:
+        print idx / 128, idx - idx / 128 * 128, idx - idx / 128 * 128
 
-endList = [i for i, c in enumerate(m) if c == 0xffffffff]
-#  for i in endList:
-    #  print (i - (ord('}') & 0x7f) * 4) / 512
 
-endList = [i for i, c in enumerate(m) if c == 501]
-print endList
-#  [80517, 151080]
-
-for i in printable:
-    if (80517 - 4 * ord(i)) / 512 in m:
-        print i
-        print (80517 - 4 * ord(i)) / 512
+'''
+    v2 = input_map[4 * (128 * v2 + *hidden_message & 127)]
+    v2 = input_map[128 * v2 + *hidden_message & 127]
+    v2 = input_map[*hidden_message & 127]
+'''
